8bd0ab0d9f6001f04c2c89922f74abf26442f451
Thucnv <nguyenvanthuc1912@gmail.com>
2020-07-09 13:47:11 +0700

    fisrt commit

---
+++ build/precache-manifest.5a8622ed5efd8f12e766598c2cae4963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d195062f81ebf823a0052d4c0f2f1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48b177602bcada0c37b4",
+    "url": "/static/css/main.72209eff.chunk.css"
+  },
+  {
+    "revision": "01a1caccc49200887166",
+    "url": "/static/js/2.c0af4aaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c0af4aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48b177602bcada0c37b4",
+    "url": "/static/js/main.7ff32793.chunk.js"
+  },
+  {
+    "revision": "9f7b1db373fef5e818d1",
+    "url": "/static/js/runtime-main.d0111201.js"
+  }
+]);
